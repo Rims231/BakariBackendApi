75cb3b85fa13a03571d5b6a3066219301bb440fe
Maigida Victor Rimam <116484191+Rims231@users.noreply.github.com>
2025-03-20 22:50:01 +0100

    Update AppDbContext.cs

--- InfrastructureLayer/Data/AppDbContext.cs
+++ InfrastructureLayer/Data/AppDbContext.cs
@@ -5,12 +5,7 @@ namespace InfrastructureLayer.Data
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        public object User;
-
-        //public DbSet<Student> Students { get; set; } 
-        //public DbSet<Payment> Payments { get; set; } = null!;
-        //public DbSet<Login> Logins { get; set; } = null!;
-        //public DbSet<Admin> Admins { get; set; } = null!;
+       
 
         public DbSet<Student> Students => Set<Student>();
         public DbSet<Admin> Admins => Set<Admin>();
@@ -18,6 +13,6 @@ namespace InfrastructureLayer.Data
         public DbSet<Payment> Payments => Set<Payment>();
         public DbSet<Login> Logins => Set<Login>();
 
-       public DbSet<UserLogin> UsersLogin => Set<UserLogin>();
+       
     }
 }
